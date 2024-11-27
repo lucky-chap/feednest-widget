460b0782c0b3842472fd0933cc3359403cbfea35
Huncho <hunchodotdev@gmail.com>
2024-11-27 09:30:13 +0000

    add files

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+// import FeedbackWidget from "./components/FeedbackWidget.tsx";
+// const userId = import.meta.env.VITE_APP_USER_ID
+
+// const USER_ID = `${import.meta.glob.VITE_APP_USER_ID}`;
+// const USER_EMAIL = `${import.meta.env.VITE_APP_USER_EMAIL}`;
+// const ORBIT_ID = `${import.meta.env.VITE_APP_ORBIT_ID}`;
+
+// The react app is good for testing out the widget and looking at how it will
+// look before publishing. You can add a dummy orbitId here and test against
+// your orbits.
+
+import { FeedbackWidget } from "../dist/index"
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+   {/* <FeedbackWidget userId='jx736m18cgqqwjg35cht2a388d6z7w2y' userEmail='hunchodotdev@gmail.com' orbitId='k978s8czxjet5xp5hfwfv28t8570wpja' /> */}
+
+{/* <FeedbackWidget userId="kn73ps1enahzbqmmj0gy6t110h70xtfg" userEmail="hunchodotdev@gmail.com" orbitId="kd73atavksyxdqdbbgkxpwp5cd70xbvj" /> */}
+
+
+{/* <FeedbackWidget userId='kn73ps1enahzbqmmj0gy6t110h70xtfg' userEmail='hunchodotdev@gmail.com' orbitId='kd73atavksyxdqdbbgkxpwp5cd70xbvj' /> */}
+{/* <FeedbackWidget userId='jx736m18cgqqwjg35cht2a388d6z7w2y' userEmail='hunchodotdev@gmail.com' orbitId='k973pj8j1pdx5qrcbt0m1pym0570y3t6' /> */}
+{/* <FeedbackWidget userId='jx736m18cgqqwjg35cht2a388d6z7w2y' userEmail='hunchodotdev@gmail.com' orbitId='k973pj8j1pdx5qrcbt0m1pym0570y3t6' /> */}
+  </StrictMode>,
+);
